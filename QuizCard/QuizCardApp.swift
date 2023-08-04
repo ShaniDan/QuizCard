d50287944fdf2947aad309ba07979139426ff640
ShaniDan <smirabzalova88@gmail.com>
2023-08-04 13:09:32 -0700

    Initial Commit

---
+++ QuizCard/QuizCardApp.swift
@@ -0,0 +1,20 @@
+//
+//  QuizCardApp.swift
+//  QuizCard
+//
+//  Created by Shakhnoza Mirabzalova on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizCardApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
